08831ca858e1d10739d5dce9cf2ec8ff9add2ee9
Tony Li <tonyli7119@gmail.com>
2024-01-07 19:15:16 -0500

    nav bar created

---
+++ src/components/NavBar.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import "./NavBar.css";
+
+function NavBar() {
+  return (
+    <>
+      <div className="navContainer">League Of Legends</div>
+    </>
+  );
+}
+
+export default NavBar;
